42c7b618443a201aeccac3311a4240bcd8c9dfda
Nahijul Islam Niloy <nniloy888@gmail.com>
2023-10-14 22:51:31 +0600

    complete new file update

--- project.cpp
+++ project.cpp
@@ -1,201 +1,168 @@
-#include<graphics.h>
+#include <graphics.h>
 using namespace std;
 int main()
 {
-    int i, gd = DETECT , gm;
-    initgraph (&gd,&gm,(char*)"");
+    int i, gd = DETECT, gm;
+    initgraph(&gd, &gm, (char *)"");
 
+    ///////////////////////BG START///////////////////////
+    int arrbgt[] = {0, 418, 0, 0, 640, 0, 640, 418, 0, 418};
+    setfillstyle(1, LIGHTGRAY);
+    fillpoly(5, arrbgt);
+    int arrbgb[] = {0, 480, 0, 419, 640, 419, 640, 480, 0, 480};
+    setfillstyle(1, LIGHTGREEN);
+    fillpoly(5, arrbgb);
+    ///////////////////////BG END///////////////////////
 
+    ////////////////BUILDING START//////////////////
+    setcolor(BLACK);
+    int arr1[] = {217, 418, 247, 386, 327, 386, 327, 418, 217, 418};
+    setfillstyle(9, DARKGRAY);
+    fillpoly(5, arr1);
 
-int poly2[12]={325,294, 350,390, 416,305, 416,386, 325,386 ,325,294};
-drawpoly(6,poly2);
+    int arr2[] = {327, 418, 327, 386, 416, 386, 416, 418, 327, 418};
+    setfillstyle(9, DARKGRAY);
+    fillpoly(5, arr2);
 
+    int arr3[] = {180, 418, 180, 332, 246, 326, 246, 385, 218, 418, 180, 418};
+    setfillstyle(1, RED);
+    fillpoly(6, arr3);
+    int arr4[] = {246, 385, 246, 326, 282, 298, 282, 385, 246, 385};
+    setfillstyle(1, RED);
+    fillpoly(5, arr4);
 
-    //cloud1
-    setfillstyle(SOLID_FILL,WHITE);
-    circle(28,73,18);
+    int arr5[] = {282, 385, 282, 298, 325, 288, 325, 385, 282, 385};
+    setfillstyle(9, LIGHTRED);
+    fillpoly(5, arr5);
+    int arr6[] = {325, 385, 325, 288, 397, 302, 397, 385, 325, 385};
+    setfillstyle(10, LIGHTBLUE);
+    fillpoly(5, arr6);
 
-    floodfill(29,74,WHITE);
-    //cloud1
-    setfillstyle(SOLID_FILL,WHITE);
-    circle(54,47,24);
-    floodfill(55,48,WHITE);
-    //cloud1
-    setfillstyle(SOLID_FILL,WHITE);
-    circle(46,85,10);
-    floodfill(47,86,WHITE);
-    //cloud1
-    setfillstyle(SOLID_FILL,WHITE);
-    circle(70,67,27);
-    floodfill(71,68,WHITE);
-    //Cloud2
-    setfillstyle(SOLID_FILL,WHITE);
-    circle(490,97,25);
-    floodfill(491,98,WHITE);
-    //cloud2
-    setfillstyle(SOLID_FILL,WHITE);
-    circle(521,98,25);
-    floodfill(522,99,WHITE);
-    //Cloud2
-    setfillstyle(SOLID_FILL,WHITE);
-    circle(552,97,25);
-    floodfill(553,98,WHITE);
-    //cloud2
-    setfillstyle(SOLID_FILL,WHITE);
-    circle(521,68,30);
-    floodfill(522,69,WHITE);
+    int arr7[] = {397, 385, 397, 302, 408, 303, 408, 385, 397, 385};
+    setfillstyle(1, LIGHTRED);
+    fillpoly(5, arr7);
 
+    int arr8[] = {408, 418, 408, 303, 416, 304, 416, 418, 408, 418};
+    setfillstyle(1, RED);
+    fillpoly(5, arr8);
+    int arr9[] = {416, 418, 416, 247, 429, 243, 429, 418, 416, 418};
+    setfillstyle(1, RED);
+    fillpoly(5, arr9);
+    int arr10[] = {429, 418, 429, 243, 466, 252, 466, 418, 429, 418};
+    setfillstyle(1, LIGHTRED);
+    fillpoly(5, arr10);
 
-    //BG
-    setfillstyle(SOLID_FILL,LIGHTGRAY);
-    rectangle(0,0,640,418);
-    floodfill(1,1,WHITE);
-    //GREEN
-    setfillstyle(SOLID_FILL,LIGHTGREEN);
-    rectangle(0,419,640,480);
-    floodfill(1,420,WHITE);
+    int arr11[] = {466, 280, 466, 252, 483, 257, 466, 280};
+    setfillstyle(1, RED);
+    fillpoly(4, arr11);
+    int arr12[] = {466, 418, 466, 280, 483, 257, 483, 418, 466, 418};
+    setfillstyle(1, LIGHTRED);
+    fillpoly(5, arr12);
+    int arr13[] = {483, 418, 483, 284, 497, 285, 497, 418, 483, 418};
+    setfillstyle(9, LIGHTRED);
+    fillpoly(5, arr13);
+    int arr14[] = {483, 284, 483, 258, 497, 261, 497, 285, 483, 284};
+    setfillstyle(1, RED);
+    fillpoly(5, arr14);
 
+    int arr15[] = {141,328, 222,323, 249,295, 320,288, 416,305,325,295, 281,298, 246,326, 180,331 ,141,328};
+    setfillstyle(9, RED);
+    fillpoly(10, arr15);
 
-
-
-setcolor(BLACK);
-line(15,418,630,418); // base line
-line(180,418,180,331);//H Line 1
-line(219,418,219,381);// L2
-line(300,418,300,356);// L5
-line(411,418,411,387);
-line(419,418,419,248);// L6
-line(435,418,435,242);// L7
-line(471,418,471,254);// L8
-line(490,418,490,257);//L9
-line(490,396,540,396);//L10
-line(540,396,556,418);//slide
-line(501,396,501,266);//L11
-line(327,386,327,295);
-//horizontal lines first floor done
-
-line(141,328,181,331);
-line(181,331,246,326);
-line(246,326,281,299);
-line(281,299,325,295);
-line(325,295,418,305);
-line(219,418,246,386);
-line(246,386,246,326);
-line(246,386,410,386);
-line(253,386,253,359);
-line(219,381,253,359);
-line(253,359,300,356);
-line(300,356,326,360);
-
-
-//SH
-line(141,328,222,323);
-line(222,323,249,295);
-line(249,295,320,288);
-line(320,288,418,305);
-line(147,318,220,312);//Top layer HZ
-line(220,312,246,285);
-line(246,285,321,279);
-line(321,279,321,288);//Hline small joint
-line(321,279,418,296);
-
-//Hlines 2nd floor
-line(141,328,141,153);
-line(147,318,147,164);
-line(147,164,177,200);
-line(176,315,176,240);
-line(325,279,325,222);
-line(400,294,400,243);
-//2nd floor wall h lines
-line(193,288,193,314);
-line(246,285,246,265);
-line(246,265,325,251);
-//V line
-line(147,293,193,288);
-line(193,288,246,285);
-
-//Top V lines
-line(176,240,207,245);
-line(207,245,324,222);
-line(324,222,419,248);
-line(419,248,435,242);
-//Top 2 V lines
-line(170,242,319,213);
-line(319,213,435,242);
-line(435,242,512,263);
-line(512,263,501,266);//Edge det small
-line(501,266,490,265);//Edge det small
-line(170,230,319,204);
-line(319,204,504,255);
-//H lines
-line(504,255,504,192);
-line(504,192,490,200);//depth
-line(512,263,512,187);
-line(319,213,319,204);
-line(170,242,170,230);
-
-//3rd floor h lines
-line(173,230,173,208);
-line(173,208,202,211);// v small
-line(202,209,202,213);// v atc
-line(173,208,316,177);//v line balc
-line(316,177,342,184);//v
-line(177,229,177,172);
-line(316,203,316,177);
-line(342,211,342,130);
-line(438,237,438,165);
-line(490,252,490,200);
-line(490,200,438,179);
-line(504,192,438,165);//v
-line(438,165,318,119);//vv
-line(318,119,147,164);//vvv
-line(177,172,342,130);
-
-
-//vvv3
-line(141,153,319,106);
-line(319,106,319,119);//hh con
-line(319,106,512,186);
+    int arr16[] = {416,305, 320,288, 320,278, 416,296  , 416,305};
+    setfillstyle(1, LIGHTRED);
+    fillpoly(5, arr16);
+    int arr17[] = {320,288, 249,295, 222,323, 141,328, 141,152, 318,106, 318,119, 149,164, 149,318, 220,312, 246,286, 320,278  ,320,288};
+    setfillstyle(1, RED);
+    fillpoly(13, arr17);
+    int arr18[] = {318,106, 511,187,511,265, 317,213,317,203, 506,257,506,192  ,318,119,318,106};
+    setfillstyle(1, LIGHTRED);
+    fillpoly(9, arr18);
+    int arr19[] = {317,213, 170,241,170,232 ,317,203, 317,213};
+    setfillstyle(1, RED);
+    fillpoly(5, arr19);
+    int arr20[] = {428,243, 415,247,324,222,207,245,177,240,317,213 , 428,243};
+    setfillstyle(9, RED);
+    fillpoly(7, arr20);
 
 
 
 
-//4th floor H lines
-line(180,142,180,99);
-line(274,117,274,66);
-line(357,121,357,100);
-line(393,136,393,116);
-line(493,179,493,166);
-//hh val
-line(273,108,319,96);
-line(319,96,357,112);
-line(393,126,493,166);
+    int arr21[] = {177,241, 208,246, 208,271, 177,274 ,177,241};
+    setfillstyle(11, DARKGRAY);
+    fillpoly(5, arr21);
+    int arr22[] = {177,274,208,271,208,314,177,316  ,177,274};
+    setfillstyle(9, LIGHTRED);
+    fillpoly(5, arr22);
+    int arr23[] = {208,314, 208,245,241,238,241,290,221,311 , 208,314};
+    setfillstyle(9, LIGHTBLUE);
+    fillpoly(6, arr23);
+    int arr24[] = {241,290, 241,266, 325,252,325,279,319,278,246,285 ,241,290};
+    setfillstyle(9, LIGHTRED);
+    fillpoly(7, arr24);
+    int arr25[] = {241,238, 281,231,281,259,241,265 ,241,238};
+    setfillstyle(11, DARKGRAY);
+    fillpoly(5, arr25);
+    int arr26[] = {281,231, 325,223,325,253,281,258 ,281,231};
+    setfillstyle(9, LIGHTCYAN);
+    fillpoly(5, arr26);
+    int arr27[] = {325,223,393,241,393,264  ,325,253,325,223};
+    setfillstyle(9, LIGHTCYAN);
+    fillpoly(5, arr27);
+    int arr28[] = {325,279,325,253,393,265,393,293  ,325,279};
+    setfillstyle(1, LIGHTRED);
+    fillpoly(5, arr28);
+    int arr29[] = {393,293,393,241,415,246,415,297 ,393,293};
+    setfillstyle(9, RED);
+    fillpoly(5, arr29);
 
-// vvr
-line(180,99,274,66);
-line(274,66,393,116);
-//r ang
-line(180,99,147,90);
-line(274,66,266,42);
-line(266,42,482,147);//v
-line(482,147,453,150);
-line(147,90,266,42);
-line(147,90,147,79);
-line(266,42,266,32);
-line(147,79,266,32);//v
-line(482,147,482,137);
-line(266,32,482,137);
+    int arr30[] = {149,293, 193,288, 193,313, 149,317 ,149,293};
+    setfillstyle(9, WHITE);
+    fillpoly(5, arr30);
+    int arr31[] = {193,288, 246,284,220,311 ,193,313,193,288};
+    setfillstyle(9, WHITE);
+    fillpoly(5, arr31);
 
 
+    ////////////2ND FLOOR///////////
+    int arr32[] = {177,230, 177,172, 341,128, 341,210,  318,203, 177,230};
+    setfillstyle(9, LIGHTRED);
+    fillpoly(6, arr32);
+    int arr33[] = {177,200,149,164,318,118,341,128,177,172,177,200 };
+    setfillstyle(9, RED);
+    fillpoly(6, arr33);
+    int arr34[] = {341,128,435,165,435,237, 341,210, 341,128 };
+    setfillstyle(1, LIGHTRED);
+    fillpoly(5, arr34);
+    int arr35[] = {435,165,504,192,492,200,435,180,435,165 };
+    setfillstyle(9, RED);
+    fillpoly(5, arr35);
+    int arr36[] = {492,200,505,191,505,256,492,252, 492,200 };
+    setfillstyle(9, RED);
+    fillpoly(5, arr36);
+    int arr37[] = {435,180, 492,200,492,234, 435,216, 435,180 };
+    setfillstyle(9, LIGHTBLUE);
+    fillpoly(5, arr37);
+    int arr38[] = {435,216,502,238,502,255,435,236, 435,216 };
+    setfillstyle(9, WHITE);
+    fillpoly(5, arr38);
 
-//window 3fr
-line(348,151,348,169);
-line(348,151,406,170);
-line(406,170,406,187);
-line(348,169,406,187);
-line(348,154,403,172);
-line(406,170,403,172);
-line(403,172,403,185);
+    //////////WINDOWS////////
+    int arr48[] = {350,170, 350,151, 406,170, 406,189 ,350,170 };
+    setfillstyle(9, RED);
+    fillpoly(5, arr48);
+    int arr49[] = {350,170,350,155, 401,172, 401,188, 350,170 };
+    setfillstyle(1, YELLOW);
+    fillpoly(5, arr49);
+    int arr50[] = {173,230,173,209,316,177,316,204, 173,230 };
+    setfillstyle(9, WHITE);
+    fillpoly(5, arr50);
+    int arr51[] = {316,204,316,177,341,184,341,209,316,204 };
+    setfillstyle(9, WHITE);
+    fillpoly(5, arr51);
+    int arr52[] = { };
+    setfillstyle(9, WHITE);
+    fillpoly(5, arr52);
 
 
 
@@ -203,6 +170,35 @@ line(403,172,403,185);
 
 
 
+    //////////////3RD FLOOR////////
+    int arr39[] = {180,142,180,99,273,65,273,117,180,142};
+    setfillstyle(9, LIGHTRED);
+    fillpoly(5, arr39);
+    int arr40[] = {180,99,147,90,266,43,273,65, 180,99};
+    setfillstyle(9, RED);
+    fillpoly(5, arr40);
+    int arr41[] = {147,90,147,78,265,32,266,43,147,90};
+    setfillstyle(1, RED);
+    fillpoly(5, arr41);
+    int arr42[] = {265,32, 482,137,482,147,266,43 ,265,32};
+    setfillstyle(1, LIGHTRED);
+    fillpoly(5, arr42);
+    int arr43[] = {266,43, 482,147, 453,150, 394,126 ,394,116, 273,65, 266,43};
+    setfillstyle(9, LIGHTRED);
+    fillpoly(7, arr43);
+
+    int arr44[] = {273,108, 273,65, 356,100, 356,112, 319,96, 273,108, 273,108};
+    setfillstyle(9, LIGHTBLUE);
+    fillpoly(7, arr44);
+    int arr45[] = {273,108, 319,96, 356,112, 356,120, 318,106, 273,117 , 273,108};
+    setfillstyle(9, WHITE);
+    fillpoly(7, arr45);
+    int arr46[] = {356,122, 356,100, 395,116, 395,138 ,356,122};
+    setfillstyle(9, RED);
+    fillpoly(5, arr46);
+    int arr47[] = {395,138, 395,126, 493,166,493,179 ,395,138};
+    setfillstyle(9, WHITE);
+    fillpoly(5, arr47);
 
 
 
@@ -210,10 +206,43 @@ line(403,172,403,185);
 
 
 
+    ////////////////BUILDING END//////////////////
 
-
-
-
+    ////////////////////CLOUDS START//////////////////
+    // cloud1
+    setcolor(WHITE);
+    setfillstyle(SOLID_FILL, WHITE);
+    circle(28, 73, 18);
+    floodfill(29, 74, WHITE);
+    // cloud1
+    setfillstyle(SOLID_FILL, WHITE);
+    circle(54, 47, 24);
+    floodfill(55, 48, WHITE);
+    // cloud1
+    setfillstyle(SOLID_FILL, WHITE);
+    circle(46, 85, 10);
+    floodfill(47, 86, WHITE);
+    // cloud1
+    setfillstyle(SOLID_FILL, WHITE);
+    circle(70, 67, 27);
+    floodfill(71, 68, WHITE);
+    // Cloud2
+    setfillstyle(SOLID_FILL, WHITE);
+    circle(490, 97, 25);
+    floodfill(491, 98, WHITE);
+    // cloud2
+    setfillstyle(SOLID_FILL, WHITE);
+    circle(521, 98, 25);
+    floodfill(522, 99, WHITE);
+    // Cloud2
+    setfillstyle(SOLID_FILL, WHITE);
+    circle(552, 97, 25);
+    floodfill(553, 98, WHITE);
+    // cloud2
+    setfillstyle(SOLID_FILL, WHITE);
+    circle(521, 68, 30);
+    floodfill(522, 69, WHITE);
+    ////////////////////CLOUDS END//////////////////
 
 
 
